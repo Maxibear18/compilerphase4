13b6bea9d07f81284fe89d2bde6537d7be5f1686
Sirr Mena <rqo7@txstate.edu>
2026-04-30 13:30:09 -0500

    added some incomplete expression logic

--- src/codegen.c
+++ src/codegen.c
@@ -22,6 +22,45 @@ void resetRegs() {
 
 
 //Expressions here
+char* expressions(struct treenode *node) {
+    if (!node){
+       return NULL;
+    }
+
+    if (node->nodeKind == INTEGER) {
+        char* r = nextReg();
+        printf("li %s, %d\n", r, node->val);
+        return r;
+    }
+
+    //enter variable logic here
+
+    if (node->numChildren == 3) {
+
+        char* left  = expr(node->children[0]);
+        char* right = expr(node->children[2]);
+        char* result   = nextReg();
+
+        int op = node->children[1]->val;
+
+        if (op == ADD) {
+            printf("add %s, %s, %s\n", result, left, right);
+        }
+        else if (op == SUB){
+            printf("sub %s, %s, %s\n", result, left, right);
+        }
+        else if (op == MUL){
+            printf("mul %s, %s, %s\n", result, left, right);
+        }
+        else if (op == DIV) {
+            printf("div %s, %s\n", left, right);
+            printf("mflo %s\n", result);
+        }
+
+    return res;
+
+    }
+}
 
 
 
