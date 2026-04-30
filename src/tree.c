a8bc027273057760a84773dd1a9b01448e3faa8e
Sirr Mena <126599112+Maxibear18@users.noreply.github.com>
2026-04-30 12:30:52 -0500

    Updated tree.c

--- src/tree.c
+++ src/tree.c
@@ -4,79 +4,182 @@
 #include<stdlib.h>
 #include<string.h>
 
-/* string values for ast node types, makes tree output more readable */
-char *nodeNames[33] = {"program", "declList", "decl", "varDecl", "typeSpecifier",
-                       "funDecl", "formalDeclList", "formalDecl", "funBody",
-                       "localDeclList", "statementList", "statement", "compoundStmt",
-                       "assignStmt", "condStmt", "loopStmt", "returnStmt","expression",
-                       "relop", "addExpr", "addop", "term", "mulop", "factor",
-                       "funcCallExpr", "argList", "integer", "identifier", "var",
-                       "arrayDecl", "char", "funcTypeName"};
+typedef struct treenode tree;
 
-char *typeNames[3] = {"int", "char", "void"};
-char *ops[10] = {"+", "-", "*", "/", "<", "<=", "==", ">=", ">", "!="};
+// the root of the ast
+tree *ast;
 
 tree *maketree(int kind) {
-      tree *this = (tree *) malloc(sizeof(struct treenode));
-      this->nodeKind = kind;
-      this->numChildren = 0;
-      return this;
+   //makes basic tree node without any values
+   tree *node = (tree*)malloc(sizeof(tree));
+   node->nodeKind = kind;
+   node->numChildren = 0;
+   node->val = 0;
+   node->name = NULL;
+   node->op = 0;
+   node->parent = NULL;
+   for (int i = 0; i < MAXCHILDREN; i++) {
+      node->children[i] = NULL;
+   }
+   return node;
 }
-
 tree *maketreeWithVal(int kind, int val) {
-      tree *this = (tree *) malloc(sizeof(struct treenode));
-      this->nodeKind = kind;
-      this->numChildren = 0;
-      this->val = val;
-      return this;
+   //makes a tree node and then adds a val to it
+   tree *node = maketree(kind);
+   node->val = val;
+   return node;
+}
+tree *maketreeWithName(int kind, const char *name) {
+   tree *node = maketree(kind);
+   node->name = strdup(name);
+   return node;
+}
+tree *maketreeWithOp(int kind, char op) {
+   tree *node = maketree(kind);
+   node->op = op;
+   return node;
 }
 
 void addChild(tree *parent, tree *child) {
-      if (parent->numChildren == MAXCHILDREN) {
-          printf("Cannot add child to parent node\n");
-          exit(1);
-      }
-      nextAvailChild(parent) = child;
-      parent->numChildren++;
+   //checks there is a parent and a child and then checks if the max
+   //children have been reached if not it assigns the child to the 
+   //parent node
+   if (!parent || !child) {
+      return;
+   }
+   if (parent->numChildren < MAXCHILDREN) {
+      parent->children[parent->numChildren++] = child;
+      child->parent = parent;
+   }
 }
+void printAst(tree *root, int nestLevel) {
+  // this function shouldn't be called with a NULL node
+  // you should be able to remove this return check
+  if (!root) return;
+  // we print nestLevel*2 spaces, then we print the number 123
+  // if nestLevel == 0 then there are 0 leading spaces
+  // if nestLevel == 1 then there are 2 leading spaces
+  // we can easily tab out print statements with this method
+  printf("%*s", nestLevel * 2, "");
 
-void printAst(tree *node, int nestLevel) {
-      char* nodeName = nodeNames[node->nodeKind];
-      if(strcmp(nodeName,"identifier") == 0){
-          if(node->val == -1)
-              printf("%s,%s\n", nodeName,"undeclared variable");
-          else
-              printf("%s,%s\n", nodeName,get_symbol_id(node->val));
-      }
-      else if(strcmp(nodeName,"integer") == 0){
-          printf("%s,%d\n", nodeName,node->val);
-      }
-      else if(strcmp(nodeName,"char") == 0){
-          printf("%s,%c\n", nodeName,node->val);
-      }
-      else if(strcmp(nodeName,"typeSpecifier") == 0){
-          printf("%s,%s\n", nodeName,typeNames[node->val]);
-      }
-      else if(strcmp(nodeName,"relop") == 0 || strcmp(nodeName,"mulop") == 0 || strcmp(nodeName,"addop") == 0){
-          printf("%s,%s\n", nodeName,ops[node->val]);
-      }
-      else{
-          printf("%s\n", nodeName);
-      }
-
-      int i, j;
-
-      for (i = 0; i < node->numChildren; i++)  {
-          for (j = 0; j < nestLevel; j++)
-              printf("    ");
-          printAst(getChild(node, i), nestLevel + 1);
-      }
-
-}
-
-void flattenList(tree *list, tree *subList){
-    for(int i=0; i < subList->numChildren; i++){
-        addChild(list,getChild(subList,i));
+  switch(root->nodeKind) {
+     case PROGRAM: 
+        printf("program\n");
+        break;
+     case DECLLIST:
+        printf("declList\n");
+        break;
+     case DECL:
+        printf("decl\n");
+        break;
+     case VARDECL:
+        printf("varDecl\n");
+        break;
+     case TYPESPEC:
+        printf("typeSpecifier,%s\n", root->name);
+        break;
+     case FUNDECL:
+        printf("funDecl\n");
+        break; 
+     case FORMALDECLLIST:
+        printf("formalDeclList\n");
+        break;
+     case FORMALDECL:
+        printf("formalDecl\n");
+        break;
+     case FUNCTYPENAME: 
+        printf("funcTypeName\n");
+        break;
+     case FUNBODY:
+        printf("funBody\n");
+        break;
+     case LOCALDECLLIST:
+        printf("localDeclList\n");
+        break;
+     case STATEMENTLIST:
+        printf("statementList\n");
+        break;
+     case STATEMENT:
+        printf("statement\n");
+        break;
+     case COMPOUNDSTMT:
+        printf("compoundStmt\n");
+        break;
+     case ASSIGNSTMT:
+        printf("assignStmt\n");
+        break;
+     case CONDSTMT:
+        printf("condStmt\n");
+        break;
+     case LOOPSTMT:
+        printf("loopStmt\n");
+        break;
+     case RETURNSTMT:
+        printf("returnStmt\n");
+        break;
+     case EXPRESSION:
+        printf("expression\n");
+        break;
+     case ADDEXPR:
+        printf("addExpr\n");
+        break;
+     case TERM:
+        printf("term\n");
+        break;
+     case FACTOR:
+        printf("factor\n");
+        break;
+     case ADDOP:
+        printf("addop,%c\n", root->op);
+        break;
+     case MULOP:
+        printf("mulop,%c\n", root->op);
+        break;
+     case RELOP:
+    switch (root->val) {
+        case LT:  printf("relop,<\n"); break;
+        case LTE: printf("relop,<=\n"); break;
+        case GT:  printf("relop,>\n"); break;
+        case GTE: printf("relop,>=\n"); break;
+        case OP_EQ:  printf("relop,==\n"); break;
+        case OP_NEQ: printf("relop,!=\n"); break;
+        default:  printf("relop\n"); break;
     }
-}
+    break;
+     case FUNCCALLEXPR:
+        printf("funcCallExpr\n");
+        break;
+     case ARGLIST:
+        printf("argList\n");
+        break;
+     case VAR:
+        printf("var\n");
+        break;
+     case ARRAYDECL:
+        printf("arrayDecl\n");
+        break;
+     case INTEGER:
+        printf("integer,%d\n", root->val);
+        break;
+     case IDENTIFIER:
+        printf("identifier,%s\n", root->name);
+        break;
+     case CHAR_NODE:
+        printf("char,%d\n", root->val);
+        break;
 
+     default:
+        printf("NODE %d\n", root->nodeKind);
+        break;
+     
+  }
+   for (int i = 0; i < root->numChildren; i++) {
+       printAst(root->children[i], nestLevel + 1);
+   }
+  // we can also print our variable spacing first ...
+  // printf("%*s", nestLevel*2, "");
+  // then print the node information
+  // printf("%s\n", "addexpr");
+  // then we print the child nodes
+  // for i from zero to root->numchildren printast(root->child[i], nestlevel+1)
+}
