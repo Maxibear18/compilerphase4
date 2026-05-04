d32e4e738e8f1271284561ad07a01858740e679c
knylander <keaton.nylander@gmail.com>
2026-05-04 04:02:05 -0500

    "null and null and null"

--- src/codegen.c
+++ src/codegen.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
 #include "tree.h"
 
-//Register logic here
+static FILE *output = NULL;
+char *expressions(struct treenode *node);
+static void assignments(struct treenode *node);
+static int labelC = 0;
 int regCount = 0;
+static void genNode(struct treenode *node);
 
+static int next(void)
+{
+    return labelC++;
+}
+
+//Register logic here
 char* nextReg() {
     static char r[10];
-    printf(r, "$t%d", regCount++);
+    snprintf(r, sizeof(r), "$t%d", regCount++);
     return r;
 }
 
@@ -18,9 +28,12 @@ void resetRegs() {
 
 //Assignments here
 
-char* assignments(struct treenode *node)
+static void assignments(struct treenode *node)
 {
+    char* rhs = expressions(node->children[1]);
+    char* var = node->children[0]->children[0]->name;
 
+    fprintf(output, "sw %s, %s\n", rhs, var);
 }
 
 
@@ -32,8 +45,13 @@ char* expressions(struct treenode *node) {
 
     if (node->nodeKind == INTEGER) {
         char* r = nextReg();
-        printf("li %s, %d\n", r, node->val);
+        fprintf(output,"li %s, %d\n", r, node->val);
         return r;
+    }
+
+    if (node->numChildren == 1) 
+    {
+        return expressions(node->children[0]);
     }
 
     //enter variable logic here
@@ -42,22 +60,22 @@ char* expressions(struct treenode *node) {
 
         char* left  = expressions(node->children[0]);
         char* right = expressions(node->children[2]);
-        char* result   = nextReg();
+        char* result = nextReg();
 
         int op = node->children[1]->val;
 
         if (op == ADD) {
-            printf("add %s, %s, %s\n", result, left, right);
+            fprintf(output, "add %s, %s, %s\n", result, left, right);
         }
         else if (op == SUB){
-            printf("sub %s, %s, %s\n", result, left, right);
+            fprintf(output, "sub %s, %s, %s\n", result, left, right);
         }
         else if (op == MUL){
-            printf("mul %s, %s, %s\n", result, left, right);
+            fprintf(output, "mul %s, %s, %s\n", result, left, right);
         }
         else if (op == DIV) {
-            printf("div %s, %s\n", left, right);
-            printf("mflo %s\n", result);
+            fprintf(output, "div %s, %s\n", left, right);
+            fprintf(output, "mflo %s\n", result);
         }
 
     return result;
@@ -72,24 +90,82 @@ char* expressions(struct treenode *node) {
 
 //Condintionals here
 
+static void conditional(struct treenode *node){
+    int lElse = next();
+    int lEnd = next();
+    char *cond = expressions(node->children[0]);
+    fprintf(output, "beq %s, $zero, L%d\n", cond, lElse);
 
-
+    genNode(node->children[1]);
+    fprintf(output, "j L%d\n", lEnd);
+    fprintf(output, "L%d:\n", lElse);
+    if(node->numChildren == 3)
+    {
+        genNode(node->children[2]);
+    }
+    fprintf(output, "L%d:\n", lEnd);
+}
 
 
 //loops here
 
+static void genNode(struct treenode *node)
+{
+    if(!node) return;
+
+    switch(node->nodeKind)
+    {
+        case PROGRAM:
+        case DECLLIST:
+        case FUNBODY:
+        case COMPOUNDSTMT:
+        case STATEMENTLIST:
+            for(int i = 0; i < node->numChildren;i++)
+            {
+                genNode(node->children[i]);
+            }
+            break;
+        case STATEMENT:
+            genNode(node->children[0]);
+            break;
+        case ASSIGNSTMT:
+            assignments(node);
+            break;
+
+        case EXPRESSION:
+        case ADDEXPR:
+        case TERM:
+        case FACTOR:
+            expressions(node);
+            break;
+        case CONDSTMT:
+            conditional(node);
+            break;
+        default:
+        for(int i = 0;i < node->numChildren; i++)
+        {
+            genNode(node->children[i]);
+        }
+        break;
+    }
+}
 
 //calling function
 void generateCode(struct treenode *root, int argc, char *argv[]) {
-    FILE *out = fopen(argv[argc - 1], "w");
-    if(!out)
+    output = fopen(argv[argc - 1], "w");
+    if(!output)
     {
         perror("fopen");
         return;
     }
 
-    fprintf(out, ".text\n");
-    fclose(out);
+    fprintf(output, ".text\n");
+    fprintf(output, ".globl main\n");
+    fprintf(output, "main:\n");
+
+    genNode(root);
+
+    fclose(output);
 }
 
 
